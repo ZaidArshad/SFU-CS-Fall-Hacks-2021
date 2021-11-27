1bc366a18fba49d79b73e2d4524f024851e282e5
Devin <devindang7@gmail.com>
2021-11-27 11:32:23 -0800

    Player Class

--- app/src/main/java/ca/sfu/bub/model/Player.java
+++ app/src/main/java/ca/sfu/bub/model/Player.java
@@ -1,4 +1,21 @@
 package ca.sfu.bub.model;
 
 public class Player {
+    // fields
+    public Piece[] pieces;
+
+    // constructors
+    public Player(Piece[] pieces){
+        this.pieces = pieces;
+    }
+
+    // getters
+    public Piece[] getPieces() {
+        return this.pieces;
+    }
+
+    // setters
+    public void setPieces(Piece[] pieces) {
+        this.pieces = pieces;
+    }
 }
