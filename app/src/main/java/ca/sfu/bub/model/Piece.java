4b057b339d48300a5d8350daa75ea77a5640a2b0
Devin <devindang7@gmail.com>
2021-11-27 11:06:40 -0800

    Piece Class

--- app/src/main/java/ca/sfu/bub/model/Piece.java
+++ app/src/main/java/ca/sfu/bub/model/Piece.java
@@ -1,4 +1,41 @@
 package ca.sfu.bub.model;
 
 public class Piece {
+    // fields
+    public int size;
+    public int owner;
+    public boolean isUsed;
+    public int xPos;
+    public int yPos;
+
+    // constructor
+    public Piece(int size, boolean isUsed, int row, int col, int owner) {
+        this.size = size;
+        this.isUsed = isUsed;
+        this.xPos = row;
+        this.yPos = col;
+        this.owner = owner;
+    }
+
+    // getters
+    public int getPlayer() {
+        return this.owner;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    // setters
+    public void setUsed(boolean isUsed) {
+        this.isUsed = isUsed;
+    }
+
+    public void setXPos(boolean isUsed) {
+        this.isUsed = isUsed;
+    }
+
+    public void setYPos(boolean isUsed) {
+        this.isUsed = isUsed;
+    }
 }
