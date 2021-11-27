d265aecfeb80cb56c1849664dbe28729158b7e84
ZaidArshad <zaidy.arshad@gmail.com>
2021-11-27 10:20:52 -0800

    initial commit

---
+++ app/src/main/java/ca/sfu/bub/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package ca.sfu.bub.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.sfu.bub.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
