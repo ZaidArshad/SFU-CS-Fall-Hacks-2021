c8743b5bbe5721398740ae343ba4bc0f200ba0ff
ZaidArshad <zaidy.arshad@gmail.com>
2021-11-27 13:55:34 -0800

    layout for game screen completed

--- app/src/main/java/ca/sfu/bub/activities/GameScreen.java
+++ app/src/main/java/ca/sfu/bub/activities/GameScreen.java
@@ -1,23 +1,43 @@
 package ca.sfu.bub.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-
-import java.util.Objects;
+import android.widget.ImageView;
 
 import ca.sfu.bub.R;
 
 public class GameScreen extends AppCompatActivity {
+    private ImageView ivPositions[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
         if (getSupportActionBar() != null) getSupportActionBar().hide();
+        populatePositions();
+    }
+
+    private void populatePositions() {
+        ivPositions = new ImageView[] {
+                findViewById(R.id.ivPos1),
+                findViewById(R.id.ivPos2),
+                findViewById(R.id.ivPos3),
+                findViewById(R.id.ivPos4),
+                findViewById(R.id.ivPos5),
+                findViewById(R.id.ivPos6),
+                findViewById(R.id.ivPos7),
+                findViewById(R.id.ivPos8),
+                findViewById(R.id.ivPos9),
+        };
+
+        for (ImageView imageView: ivPositions) {
+            imageView.setBackground(AppCompatResources.getDrawable(this, R.drawable.player2_circle));
+        }
+
     }
 
     public static Intent makeIntent(Context context) {
